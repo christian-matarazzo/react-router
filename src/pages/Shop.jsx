8f0f58600bdeea0de28741b76e0d5f52b2dc66b3
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-20 17:55:12 +0100

    add code polishing

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -34,9 +34,6 @@ export default function Shop() {
                             <div className="card-body">
                                 <h5 className="card-title">{product.title}</h5>
                                 <h6 className="card-subtitle mb-2 ">{`${product.price} €`}</h6>
-{/*                                 <p className="card-text">
-                                    {product.description}
-                                </p> */}
                             </div>
                         </div>
                     </div>
