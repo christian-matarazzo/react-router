ef725fe3722d60014bb95f45f7d3c097bb686acf
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-20 16:45:12 +0100

    add shop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
         <main>
-            <h1>Home</h1>
+            <div className="container-fluid bg-light shadow-sm">
+                <div className="container">
+                    <div className="row ">
+                    <div className="h1 ">Welcome to Reacta.</div>
+                        <div className="col">
+                            <p className="h3">Your new look at just one click away.</p>
+                            <Link to="/shop" className="btn btn-dark btn-lg px-4 me-2 my-3">Buy now</Link>
+                        </div>
+                        <div className="col">
+                           <img src="/lady-commerce.png" className="w-50 mx-auto" alt="" />
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
-    
+
     )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,56 @@
+import axios from "axios"
+import {useState, useEffect} from "react"
+
 export default function Shop() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+            .then(response => {
+                console.log("Sono il pacchetto", response.data);
+                setProducts(response.data)
+    
+            })
+            .catch(error => {
+                console.error(error)
+            })
+
+    }, [])
+
+
+
+
+
+
     return (
         <main>
-            <h1>Shop</h1>
+            <div className="container py-5">
+                <h1 className="text-center">Our merch: </h1>
+                <div className="row g-4 justify-content-center" >
+                {products.map(product => 
+                    <div className="col col-12 col-md-6 col-lg-4 d-flex justify-content-center" key={product.id}>
+                        <div className="card d-flex flex-column"  style={{ width: '16rem'}}>
+                            <img
+                                src={product.image}
+                                className="card-img-top h-100 w-100"
+                                style={{objectFit: 'contain'}}
+                                alt="Card cap"
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title}</h5>
+                                <h6 className="card-subtitle mb-2 ">{`${product.price} €`}</h6>
+{/*                                 <p className="card-text">
+                                    {product.description}
+                                </p> */}
+                            </div>
+                        </div>
+                    </div>
+                )}
+                </div>
+
+            </div>
         </main>
     )
 }
+
