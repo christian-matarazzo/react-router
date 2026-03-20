9690083d8ad33c7422a1a205c666cb5fe003bb7c
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-20 12:47:36 +0100

    add routing and components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import axios from 'axios'
+import DefaultLayout from './layouts/DefaultLayout'
+import AboutUs from './pages/AboutUs'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout></DefaultLayout>}>
+        <Route path= "/" element={<Home />}></Route>
+        <Route path= "/about-us" element={<AboutUs />}></Route>
+        <Route path= "/shop" element= {<Shop />}></Route>
+        </Route>
+        
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
