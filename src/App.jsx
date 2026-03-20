f94e1a5b64ca4499a2aea912fdc10a2aa0d8d014
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-20 12:25:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import axios from 'axios'
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
